e7924233087a2a4b3b819505682a4932d452b3f6
Caresa Huddleston <caresa.huddleston@gmail.com>
2014-07-18 18:26:53 -0500

    created private video params method

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Metube::Application.routes.draw do
   post '/videos' => 'videos#create'
 
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
